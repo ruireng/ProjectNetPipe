962c3f06ee1b87577d1967f32ff3b654a57ff15b
Roy Liu <113428371+ruireng@users.noreply.github.com>
2023-11-25 21:43:37 +0100

    finished CipherStreams + 5/5 tests done!

--- Task2/SessionCipher.java
+++ Task2/SessionCipher.java
@@ -1,24 +1,31 @@
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+
+import javax.crypto.Cipher;
 import javax.crypto.CipherInputStream;
 import javax.crypto.CipherOutputStream;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
 import javax.crypto.spec.IvParameterSpec;
 
 public class SessionCipher {
 
     SessionKey sk;
     IvParameterSpec iv;
+    Cipher cipher;
 
     // constructor to create a SessionCipher from a SessionKey
     // the IV is created automatically
     public SessionCipher(SessionKey key) {
         sk = key;
-        int keyLength = key.getKeyBytes().length;
 
         // I learned how I should handle and create IV's from this website:
         // https://www.novixys.com/blog/java-aes-example/#3_Generate_an_Initialization_Vector_IV
-        byte[] initVector = new byte[keyLength];
+        byte[] initVector = new byte[128/8];
         SecureRandom sr = new SecureRandom();
         sr.nextBytes(initVector);
         iv = new IvParameterSpec(initVector);
@@ -40,19 +47,37 @@ public class SessionCipher {
         return iv.getIV();
     }
 
+    // epic documentation for the Cipher class:
+    // https://docs.oracle.com/javase/8/docs/api/javax/crypto/Cipher.html
+
     // attach OutputStream to which encrypted data will be written
     // return result as a CipherOutputStream instance
-    CipherOutputStream openEncryptedOutputStream(OutputStream os) {
+    CipherOutputStream openEncryptedOutputStream(OutputStream os) throws Exception {
+        // list of exceptions if it becomes important later:
+        // NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameter
+
+        // specified in the instructions:
+        // AES, CTR mode, no padding
+        cipher = Cipher.getInstance("AES/CTR/NoPadding");
+
+        // specified by the comments the professor left above the function:
+        // this cipher is for encryption
+        SecretKey key = sk.getSecretKey();
+        cipher.init(Cipher.ENCRYPT_MODE, key, iv);
         
-        CipherOutputStream cos = new CipherOutputStream(os, null);
+        CipherOutputStream cos = new CipherOutputStream(os, cipher);
 
         return cos;
     }
 
     // attach InputStream from which decrypted data will be read
     // return result as a CipherInputStream instance
-    CipherInputStream openDecryptedInputStream(InputStream is) {
-        CipherInputStream cis = new CipherInputStream(is, null);
+    CipherInputStream openDecryptedInputStream(InputStream is) throws Exception {
+        cipher = Cipher.getInstance("AES/CTR/NoPadding");
+        SecretKey key = sk.getSecretKey();
+        cipher.init(Cipher.DECRYPT_MODE, key, iv);
+
+        CipherInputStream cis = new CipherInputStream(is, cipher);
 
         return cis;
     }
