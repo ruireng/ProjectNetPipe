070272ac7e71fc7e2c6c0c61b7deb71ec6bfae49
Roy Liu <113428371+ruireng@users.noreply.github.com>
2023-11-27 15:34:25 +0100

    seems to give intended output now

--- Task3/FileDigest.java
+++ Task3/FileDigest.java
@@ -1,4 +1,35 @@
+import java.io.*;
+import java.util.Base64;
+
 public class FileDigest {
     public static void main(String[] args) {
+        String filePath = args[0];
+
+        try {
+            InputStream is = new FileInputStream(filePath);
+            byte[] buffer = new byte[1];
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+            while(is.read(buffer) != -1) {
+                baos.write(buffer);
+            }
+
+            byte[] data = baos.toByteArray();
+
+            HandshakeDigest hd = new HandshakeDigest();
+            hd.update(data);
+
+            byte[] digest = hd.digest();
+
+            String decodedDigest = Base64.getEncoder().encodeToString(digest);
+
+            System.out.println(decodedDigest);
+
+            is.close();
+            baos.close();
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 }
