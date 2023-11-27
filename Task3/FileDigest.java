68883341b41bc81a25ef5dd18eb2c93dbd0a5751
Roy Liu <113428371+ruireng@users.noreply.github.com>
2023-11-27 21:03:37 +0100

    updated code with comments

--- Task3/FileDigest.java
+++ Task3/FileDigest.java
@@ -10,21 +10,20 @@ public class FileDigest {
             byte[] buffer = new byte[1];
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
 
+            // read 1 byte at a time because it worked in IK1203 :)
             while(is.read(buffer) != -1) {
                 baos.write(buffer);
             }
 
+            // create a digest from the buffer
             byte[] data = baos.toByteArray();
-
             HandshakeDigest hd = new HandshakeDigest();
             hd.update(data);
-
             byte[] digest = hd.digest();
 
+            // encode with base64 and print out
             String decodedDigest = Base64.getEncoder().encodeToString(digest);
-
             System.out.println(decodedDigest);
-
             is.close();
             baos.close();
         }
