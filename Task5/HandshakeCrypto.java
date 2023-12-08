20b96af70add89fe72ec5439c482f93b8ab7da71
Roy Liu <113428371+ruireng@users.noreply.github.com>
2023-12-09 00:02:16 +0100

    constructors done

--- Task5/HandshakeCrypto.java
+++ Task5/HandshakeCrypto.java
@@ -1,15 +1,37 @@
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+
 public class HandshakeCrypto {
+
+    boolean certificate;
+    PublicKey pubkey;
+    PrivateKey prikey;
 
 	// constructor to create an instance for encryption/decryption with a public key
 	// the public key is given as a X509 certificate
 	public HandshakeCrypto(HandshakeCertificate handshakeCertificate) {
-
+        certificate = true;
+        pubkey = handshakeCertificate.getCertificate().getPublicKey();
+        prikey = null;
 	}
 
     // constructor to create an instance for encryption/decryption with a private key
     // the private key is given as a byte array in PKCS8/DER format
-	public HandshakeCrypto(byte[] keybytes) {
-
+	public HandshakeCrypto(byte[] keybytes) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        
+        // useful recommendation by our professor:
+        // https://stackoverflow.com/questions/20119874/how-to-load-the-private-key-from-a-der-file-into-java-private-key-object
+        
+        certificate = false;
+        pubkey = null;
+        
+        PKCS8EncodedKeySpec pkcs8 = new PKCS8EncodedKeySpec(keybytes);
+        KeyFactory kf = KeyFactory.getInstance("RSA");
+        prikey = kf.generatePrivate(pkcs8);
 	}
 
     // decrypt byte array with the key, return result as a byte array
