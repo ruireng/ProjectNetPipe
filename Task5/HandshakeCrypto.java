d21f4b0b882f594f74e27baa12384d714081e611
Roy Liu <113428371+ruireng@users.noreply.github.com>
2023-12-09 14:09:59 +0100

    all tests done! why did the format get messed up...

--- Task5/HandshakeCrypto.java
+++ Task5/HandshakeCrypto.java
@@ -1,9 +1,15 @@
+import java.security.InvalidKeyException;
 import java.security.KeyFactory;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 
 public class HandshakeCrypto {
 
@@ -35,12 +41,36 @@ public class HandshakeCrypto {
 	}
 
     // decrypt byte array with the key, return result as a byte array
-    public byte[] decrypt(byte[] ciphertext) {
-		return new byte[0];
+    public byte[] decrypt(byte[] ciphertext) 
+    throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException,
+    BadPaddingException {
+      Cipher cipher = Cipher.getInstance("RSA");
+      if(certificate) {
+        cipher.init(Cipher.DECRYPT_MODE, pubkey);
+      }
+      else {
+        cipher.init(Cipher.DECRYPT_MODE, prikey);
+      }
+
+      byte[] plaintext = cipher.doFinal(ciphertext);
+      
+		  return plaintext;
     }
 
     // encrypt byte array with the key, return result as a byte array
-    public byte [] encrypt(byte[] plaintext) {
-		return new byte[0];
+    public byte [] encrypt(byte[] plaintext) 
+    throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException,
+    BadPaddingException {
+      Cipher cipher = Cipher.getInstance("RSA");
+      if(certificate) {
+        cipher.init(Cipher.ENCRYPT_MODE, pubkey);
+      }
+      else {
+        cipher.init(Cipher.ENCRYPT_MODE, prikey);
+      }
+
+      byte[] ciphertext = cipher.doFinal(plaintext);
+
+		  return ciphertext;
     }
 }
